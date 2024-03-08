36873a561b282ebc265c7cf5949571ffc8ccfdf1
Lukáš Urban <lukas.urbann@seznam.cz>
2024-03-08 21:13:59 +0100

    added testing scene

--- project/Assets/_Scripts/ButtonActions/Exit.cs
+++ project/Assets/_Scripts/ButtonActions/Exit.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace Game.ButtonActions
 {
+    /// <summary>
+    /// Generic exit button script. Can exit the game, wow.
+    /// </summary>
     public class Exit : MonoBehaviour, IButton
     {
         [SerializeField] bool exitToDesktop = false;
