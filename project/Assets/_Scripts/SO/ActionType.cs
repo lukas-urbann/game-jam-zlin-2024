9fdef05ebb14945a6ae2bfb746ac2eb245777b3b
Lukáš Urban <lukas.urbann@seznam.cz>
2024-03-08 21:27:32 +0100

    new scriptable objects

---
+++ project/Assets/_Scripts/SO/ActionType.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Action Type", menuName = "Custom Objects/Action Type")]
+public class ActionType : ScriptableObject
+{
+    // lol
+}
---
+++ project/Assets/_Scripts/SO/ValueIdentifier.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Value Identifier", menuName = "Custom Objects/Value Identifier")]
+public class ValueIdentifier : ScriptableObject
+{
+
+}
