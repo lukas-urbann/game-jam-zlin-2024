468e6bf901f3f47ba1df4fc61e0656bc644504e9
Lukáš Urban <lukas.urbann@seznam.cz>
2024-03-09 20:05:19 +0100

    fztugvbhknj

--- project/Assets/_Scripts/Interactors/Interact_Event.cs
+++ project/Assets/_Scripts/Interactors/Interact_Event.cs
@@ -30,9 +30,8 @@ namespace Game.Interactors
         public void Interact()
         {
             if (!interactableDrunk && !interactableSober) return;
-            else if (!interactableSober && CustomIdentifier.Value == -1) return;
-            else if (!interactableDrunk && CustomIdentifier.Value == 1) return;
-
+            else if (!interactableSober && CustomIdentifier.Value == 1) return;
+            else if (!interactableDrunk && CustomIdentifier.Value == -1) return;
 
             if (requiredItemsToInteract.Count <= 0)
             {
